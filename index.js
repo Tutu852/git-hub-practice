8c867fa805b5ad68e30d0254bcea610daa41f586
rajesh <852tutukumar@gmail.com>
2025-01-15 19:22:06 +0530

    add the value

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const a  = 25;
-const name = "Rajesh kumar"
+const name1 = "Rajesh kumar"
 const dob ="15/04/1998"
 
 function sayName(){
     console.log('My Name is ' + name);
+}
+
+function isAbove18(){
+    return age >= 18;
 }
