09ddce356f860cf479d942f2e86dfdc13d92f4ee
rajesh <852tutukumar@gmail.com>
2025-01-16 18:35:37 +0530

    new changes

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function sayName(){
 function isAbove18(){
     return age >= 19;
 }
+
+ 
+function newFeaturreAdded(){
+    const name = "rajesh";
+    function inner(){
+        console.log(name);
+    }
+    return inner();
+}
+newFeaturreAdded();
